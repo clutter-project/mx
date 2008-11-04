d1899b4e876143c0e277d16b53e378ca3cbe00a7
Thomas Wood <thomas@localhost.(none)>
2008-11-04 15:03:52 +0000

    Add clutter script test application

---
+++ tests/test-script.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <glib.h>
+
+#include <clutter/clutter.h>
+
+static ClutterScript *script = NULL;
+static guint merge_id = 0;
+
+
+int
+main (int argc, char *argv[])
+{
+  GObject *stage, *blue_button, *red_button;
+  GError *error = NULL;
+  gint res;
+
+  clutter_init (&argc, &argv);
+
+  script = clutter_script_new ();
+  g_assert (CLUTTER_IS_SCRIPT (script));
+
+  clutter_script_load_from_file (script, "test-script.json", &error);
+  if (error)
+    {
+      g_print ("*** Error:\n"
+               "***   %s\n", error->message);
+      g_error_free (error);
+      g_object_unref (script);
+      return EXIT_FAILURE;
+    }
+
+  clutter_script_connect_signals (script, NULL);
+
+  res = clutter_script_get_objects (script,
+                                    "main-stage", &stage,
+                                    "red-button", &red_button,
+                                    "blue-button", &blue_button,
+                                    NULL);
+//  g_assert (res == 3);
+
+  clutter_actor_show (CLUTTER_ACTOR (stage));
+
+  clutter_main ();
+
+  g_object_unref (script);
+
+  return EXIT_SUCCESS;
+}
