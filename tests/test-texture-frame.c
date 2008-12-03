120dddc8513dc41df103f9396a0e26918439590f
Thomas Wood <thomas@linux.intel.com>
2008-12-03 17:26:30 +0000

    Add test for NbtkTextureFrame

---
+++ tests/test-texture-frame.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <clutter/clutter.h>
+#include <nbtk.h>
+
+int
+main (int argc, char *argv[])
+{
+  NbtkWidget *button;
+  ClutterColor stage_color =  { 0xff, 0xff, 0xff, 0xff };
+  ClutterActor *stage, *texture, *frame;
+  GError *err = NULL;
+
+  clutter_init (&argc, &argv);
+
+  stage = clutter_stage_get_default ();
+  clutter_actor_set_size (stage, 500, 300);
+
+  texture = clutter_texture_new_from_file ("rounded-corner.png", &err);
+
+  if (err)
+    {
+      g_critical (err->message);
+      return 1;
+    }
+
+  frame = nbtk_texture_frame_new (CLUTTER_TEXTURE (texture), 25, 25, 25, 25);
+  clutter_actor_set_position (frame, 50, 50);
+  clutter_actor_set_size (frame, 400, 200);
+
+  clutter_container_add (CLUTTER_CONTAINER (stage), frame, NULL);
+
+  clutter_actor_show (stage);
+
+  clutter_main ();
+
+  return EXIT_SUCCESS;
+}
