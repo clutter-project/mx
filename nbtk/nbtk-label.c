4be1644505a1f5caccc55de6b6744dfcb89b4d09
Thomas Wood <thomas@linux.intel.com>
2009-02-11 16:13:49 +0000

    NbtkLabel: add a padding property to NbtkLabel
    Added a padding property to allow padding between the label text and the
    border of the label.

--- nbtk/nbtk-label.c
+++ nbtk/nbtk-label.c
@@ -58,9 +58,15 @@ enum
 struct _NbtkLabelPrivate
 {
   ClutterActor *label;
+
+  NbtkPadding padding;
 };
 
-G_DEFINE_TYPE (NbtkLabel, nbtk_label, NBTK_TYPE_WIDGET);
+static void nbtk_stylable_iface_init (NbtkStylableIface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (NbtkLabel, nbtk_label, NBTK_TYPE_WIDGET,
+                         G_IMPLEMENT_INTERFACE (NBTK_TYPE_STYLABLE,
+                                                nbtk_stylable_iface_init));
 
 static void
 nbtk_label_set_property (GObject      *gobject,
@@ -106,6 +112,7 @@ static void
 nbtk_label_style_changed (NbtkWidget *self)
 {
   NbtkLabelPrivate *priv = NBTK_LABEL (self)->priv;
+  NbtkPadding *padding = NULL;
   ClutterColor *color = NULL;
   gchar *font_name;
   gchar *font_string;
@@ -115,12 +122,19 @@ nbtk_label_style_changed (NbtkWidget *self)
                      "color", &color,
                      "font-family", &font_name,
                      "font-size", &font_size,
+                     "padding", &padding,
                      NULL);
 
   if (color)
     {
       clutter_text_set_color (CLUTTER_TEXT (priv->label), color);
       clutter_color_free (color);
+    }
+
+  if (padding)
+    {
+      priv->padding = *padding;
+      g_boxed_free (NBTK_TYPE_PADDING, padding);
     }
 
   if (font_name || font_size)
@@ -157,6 +171,12 @@ nbtk_label_get_preferred_width (ClutterActor *actor,
   clutter_actor_get_preferred_width (priv->label, for_height,
                                      min_width_p,
                                      natural_width_p);
+
+  if (min_width_p)
+    *min_width_p += priv->padding.left + priv->padding.right;
+
+  if (natural_width_p)
+    *natural_width_p += priv->padding.left + priv->padding.right;
 }
 
 static void
@@ -170,6 +190,12 @@ nbtk_label_get_preferred_height (ClutterActor *actor,
   clutter_actor_get_preferred_height (priv->label, for_width,
                                       min_height_p,
                                       natural_height_p);
+
+  if (min_height_p)
+    *min_height_p += priv->padding.top + priv->padding.bottom;
+
+  if (natural_height_p)
+    *natural_height_p += priv->padding.top + priv->padding.bottom;
 }
 
 static void
@@ -183,12 +209,12 @@ nbtk_label_allocate (ClutterActor          *actor,
 
   parent_class = CLUTTER_ACTOR_CLASS (nbtk_label_parent_class);
   parent_class->allocate (actor, box, absolute_origin_changed);
-  
-  child_box.x1 = 0;
-  child_box.y1 = 0;
-  child_box.x2 = box->x2 - box->x1;
-  child_box.y2 = box->y2 - box->y1;
-  
+
+  child_box.x1 = priv->padding.left;
+  child_box.y1 = priv->padding.top;
+  child_box.x2 = box->x2 - box->x1 - priv->padding.right;
+  child_box.y2 = box->y2 - box->y1 - priv->padding.bottom;
+
   clutter_actor_allocate (priv->label, &child_box, absolute_origin_changed);
 }
 
@@ -202,6 +228,25 @@ nbtk_label_paint (ClutterActor *actor)
   parent_class->paint (actor);
 
   clutter_actor_paint (priv->label);
+}
+
+static void
+nbtk_stylable_iface_init (NbtkStylableIface *iface)
+{
+  static gboolean is_initialized = FALSE;
+
+  if (G_UNLIKELY (!is_initialized))
+    {
+      GParamSpec *pspec;
+
+      pspec = g_param_spec_boxed ("padding",
+                                  "Padding",
+                                  "Padding between the widgets borders "
+                                  "and its content",
+                                  NBTK_TYPE_PADDING,
+                                  G_PARAM_READWRITE);
+      nbtk_stylable_iface_install_property (iface, NBTK_TYPE_LABEL, pspec);
+    }
 }
 
 static void
