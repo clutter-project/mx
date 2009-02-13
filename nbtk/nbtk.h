0f84563539f11f1f3ca00535a7f9a96f14c07de3
Emmanuele Bassi <ebassi@gnome.org>
2009-02-13 16:28:09 +0000

    Remove unused header

--- nbtk/nbtk.h
+++ nbtk/nbtk.h
@@ -26,7 +26,6 @@
 #include <nbtk/nbtk-adjustment.h>
 #include <nbtk/nbtk-bin.h>
 #include <nbtk/nbtk-button.h>
-#include <nbtk/nbtk-depth-group.h>
 #include <nbtk/nbtk-draggable.h>
 #include <nbtk/nbtk-droppable.h>
 #include <nbtk/nbtk-entry.h>
