12aab5258d68a929c54307f85b5a41790bf1fbaa
Thomas Wood <thomas.wood@intel.com>
2009-04-06 12:45:53 +0100

    [test-expander] add a test for the new NbtkExpander widget

---
+++ tests/test-expander.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <clutter/clutter.h>
+#include <nbtk/nbtk.h>
+
+void
+stage_size_notify_cb (ClutterActor *stage,
+                      GParamSpec *pspec,
+                      ClutterActor *table)
+{
+  guint width, height;
+
+  clutter_actor_get_size (stage, &width, &height);
+}
+
+int
+main (int argc, char *argv[])
+{
+  NbtkWidget *expander, *scroll, *grid;
+  ClutterActor *stage;
+  int i;
+
+  clutter_init (&argc, &argv);
+
+  nbtk_style_load_from_file (nbtk_style_get_default (),
+                             "style/default.css", NULL);
+
+  stage = clutter_stage_get_default ();
+  clutter_actor_set_size (stage, 640, 480);
+  clutter_stage_set_user_resizable (CLUTTER_STAGE (stage), TRUE);
+
+  expander = nbtk_expander_new ();
+  nbtk_expander_set_label (NBTK_EXPANDER (expander), "Expander");
+  clutter_container_add_actor (CLUTTER_CONTAINER (stage),
+                               CLUTTER_ACTOR (expander));
+  clutter_actor_set_position (CLUTTER_ACTOR (expander), 10, 10);
+
+  scroll = (NbtkWidget *) nbtk_scroll_view_new ();
+  clutter_container_add_actor (CLUTTER_CONTAINER (expander),
+                               CLUTTER_ACTOR (scroll));
+  clutter_actor_set_size (CLUTTER_ACTOR (scroll), 320, 240);
+
+  grid = nbtk_grid_new ();
+  clutter_container_add_actor (CLUTTER_CONTAINER (scroll),
+                               CLUTTER_ACTOR (grid));
+
+  for (i = 1; i <= 50; i++)
+    {
+      NbtkWidget *button;
+      gchar *label;
+
+      label = g_strdup_printf ("Button %d", i);
+
+      button = nbtk_button_new_with_label (label);
+      clutter_container_add_actor (CLUTTER_CONTAINER (grid),
+                                   CLUTTER_ACTOR (button));
+
+      g_free (label);
+    }
+
+  g_signal_connect (stage, "notify::width",
+                    G_CALLBACK (stage_size_notify_cb), expander);
+  g_signal_connect (stage, "notify::height",
+                    G_CALLBACK (stage_size_notify_cb), expander);
+  clutter_actor_show (stage);
+  clutter_main ();
+
+  return EXIT_SUCCESS;
+}
