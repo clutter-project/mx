3b796a1b7fcfd2f453e09f413285fc02ad4223dd
Thomas Wood <thomas.wood@intel.com>
2009-06-22 10:56:03 +0100

    [NbtkTable] Remove deprecated API

--- nbtk/nbtk-table.h
+++ nbtk/nbtk-table.h
@@ -96,17 +96,6 @@ void        nbtk_table_add_actor_with_properties (NbtkTable    *table,
                                                   gint          column,
                                                   const gchar  *first_property_name,
                                                   ...);
-#ifndef NBTK_DISABLE_DEPRECATED
-G_GNUC_DEPRECATED void        nbtk_table_add_widget         (NbtkTable *table, NbtkWidget *widget, gint row, gint column);
-G_GNUC_DEPRECATED void        nbtk_table_add_widget_full    (NbtkTable *table, NbtkWidget *widget, gint row, gint column, gint rowspan, gint colspan, NbtkTableChildOptions options, gdouble xalign, gdouble yalign);
-G_GNUC_DEPRECATED void        nbtk_table_add_actor_full     (NbtkTable *table, ClutterActor *actor, gint row, gint column, gint rowspan, gint colspan, NbtkTableChildOptions options, gdouble xalign, gdouble yalign);
-G_GNUC_DEPRECATED void        nbtk_table_set_widget_colspan (NbtkTable *table, NbtkWidget *widget, gint colspan);
-G_GNUC_DEPRECATED void        nbtk_table_set_widget_rowspan (NbtkTable *table, NbtkWidget *widget, gint rowspan);
-G_GNUC_DEPRECATED void        nbtk_table_insert_actor_at_position (NbtkTable *table, ClutterActor *actor, gint x, gint y);
-G_GNUC_DEPRECATED void        nbtk_table_set_padding        (NbtkTable *table, const NbtkPadding *padding);
-G_GNUC_DEPRECATED void        nbtk_table_get_padding        (NbtkTable *table, NbtkPadding *padding);
-
-#endif
 G_END_DECLS
 
 #endif /* __NBTK_TABLE_H__ */
