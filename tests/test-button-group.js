c06f6764e839cd40f313e2b08d6edc65f776673c
Thomas Wood <thomas.wood@intel.com>
2009-11-03 10:39:19 +0000

    [tests] Add a test for button group

---
+++ tests/test-button-group.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env gjs
+/*
+ * Copyright 2009 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+const Clutter = imports.gi.Clutter;
+const Mx = imports.gi.Mx;
+
+
+function add_button (name, box, group)
+{
+  let button = new Mx.Button ({label: name })
+  button.toggle_mode = true;
+  box.add_actor (button);
+  group.add (button)
+}
+
+Clutter.init (0, null);
+
+let stage = Clutter.Stage.get_default ();
+stage.title = "Test Toolkit"
+
+let group = new Mx.ButtonGroup ()
+
+let box = new Mx.BoxLayout ()
+stage.add_actor (box)
+
+add_button ("Entry", box, group)
+add_button ("Table", box, group)
+add_button ("Scroll View", box, group)
+add_button ("Progress Bar", box, group)
+add_button ("Drag and Drop", box, group)
+add_button ("Expander", box, group)
+add_button ("Combo Box", box, group)
+
+group.connect ("notify::active-button",
+               function (g, p) { if (g.active_button)
+                                  stage.title = g.active_button.label
+                                 else
+                                  stage.title = "" } )
+
+stage.show ();
+Clutter.main ();
+
+stage.destroy ();
