f0fd64dda144a0d2802ad6dd663d2786e1e86948
Thomas Wood <thomas.wood@intel.com>
2009-11-13 15:35:06 +0000

    [tests] add a test for the toggle switch

---
+++ tests/test-toggle-switch.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env gjs
+/*
+ * Copyright 2009 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+const Clutter = imports.gi.Clutter;
+const Mx = imports.gi.Mx;
+
+Clutter.init (0, null);
+
+let stage = Clutter.Stage.get_default ();
+stage.title = "Test Toggle";
+
+let toggle = new Mx.Toggle ();
+toggle.set_position (10, 10);
+stage.add_actor (toggle);
+
+stage.connect ("key-press-event",
+ function (o,e) {toggle.active = !toggle.active} );
+
+stage.show ();
+Clutter.main ();
+
+stage.destroy ();
