ff4f3c4805f9dee8bcc48830e3c7f9b38a1a1fc1
Chris Lord <chris@linux.intel.com>
2010-04-06 13:21:03 +0100

    viewport: Set step-increment on adjustments
    When sync-adjustments is set, make sure to set a value on
    step-increment, otherwise it defaults to zero and the mouse-wheel
    scrolling won't work.
    
    Fixes Meego bug #343

--- mx/mx-viewport.c
+++ mx/mx-viewport.c
@@ -288,6 +288,7 @@ mx_viewport_allocate (ClutterActor          *self,
                         "page-size", available_width,
                         "upper", natural_width,
                         "page-increment", available_width / 3,
+                        "step-increment", available_width / 12,
                         NULL);
 
           /* Make sure value is clamped */
@@ -302,6 +303,7 @@ mx_viewport_allocate (ClutterActor          *self,
                         "page-size", available_height,
                         "upper", natural_height,
                         "page-increment", available_height / 3,
+                        "step-increment", available_height / 12,
                         NULL);
 
           prev_value = mx_adjustment_get_value (priv->vadjustment);
