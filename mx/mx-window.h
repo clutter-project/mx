39f1380885d0eef0a06c3129c83abd3c43b2c529
Thomas Wood <thomas.wood@intel.com>
2010-04-26 14:53:43 +0100

    Add missing copyright headers

--- mx/mx-window.h
+++ mx/mx-window.h
@@ -1,4 +1,28 @@
-/* mx-window.h */
+/*
+ * mx-window.h
+ *
+ * Copyright 2010 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * Boston, MA 02111-1307, USA.
+ *
+ * Written by: Thomas Wood <thomas.wood@intel.com>
+ *             Chris Lord <chris@linux.intel.com>
+ *
+ */
+
+
 
 #if !defined(MX_H_INSIDE) && !defined(MX_COMPILATION)
 #error "Only <mx/mx.h> can be included directly.h"
