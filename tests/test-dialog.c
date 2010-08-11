8f77a915075ae4c5bc3c6fca58ff01f828419884
iain <iain@linux.intel.com>
2010-08-11 10:19:22 -0700

    Add a test for MxDialog

---
+++ tests/test-dialog.c
@@ -0,0 +1,54 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright 2010 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "test-mx.h"
+
+static void
+close_dialog_cb (gpointer      unused,
+                 ClutterActor *dialog)
+{
+  clutter_actor_hide (dialog);
+}
+
+void
+dialog_main (ClutterContainer *group)
+{
+  ClutterActor *stage, *launch, *dialog, *label;
+  MxAction *action;
+
+  launch = mx_button_new_with_label ("Launch dialog");
+  dialog = mx_dialog_new ();
+
+  clutter_actor_set_position (launch, 50, 50);
+  clutter_container_add (group, launch, NULL);
+  g_signal_connect_swapped (launch, "clicked",
+                            G_CALLBACK (clutter_actor_show), dialog);
+
+  label = mx_label_new_with_text ("This is a model dialog");
+  mx_dialog_set_content (MX_DIALOG (dialog), label);
+
+  action = mx_action_new_full ("close", "Close dialog",
+                               G_CALLBACK (close_dialog_cb), dialog);
+  mx_dialog_add_action (MX_DIALOG (dialog), action);
+
+  stage = clutter_actor_get_stage (CLUTTER_ACTOR (group));
+  mx_modal_frame_set_transient_parent (MX_MODAL_FRAME (dialog),
+                                       stage ? stage : CLUTTER_ACTOR (group));
+}
--- tests/test-mx.c
+++ tests/test-mx.c
@@ -209,6 +209,8 @@ main (int argc, char *argv[])
            (GCallback) styles_main, CLUTTER_CONTAINER (holder));
   add_tab (CLUTTER_CONTAINER (vbox), group, "Modal Frame",
            (GCallback) modal_frame_main, CLUTTER_CONTAINER (holder));
+  add_tab (CLUTTER_CONTAINER (vbox), group, "Dialog",
+           (GCallback) dialog_main, CLUTTER_CONTAINER (holder));
 
   prev = mx_action_new_full ("Previous tab",
                              "Previous tab",
